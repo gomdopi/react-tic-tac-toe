8309149b7ddecee168882efab2a3f65a456b27a4
Daniel Kim <io.gomdopi@gmail.com>
2020-08-02 00:08:33 -0700

    up to step 3 of react extra practice

--- src/index.js
+++ src/index.js
@@ -1,17 +1,176 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+function Square(props) {
+  return (
+    <button
+      className="square"
+      onClick={props.onClick}
+    >
+      {props.value}
+    </button>
+  );
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    );
+  }
+
+  render() {
+    let boardSquares = [];
+    for (let row = 0; row < 3; row++) {
+      let boardRow = [];
+      for (let col = 0; col < 3; col++) {
+        boardRow.push(this.renderSquare((row * 3) + col));
+      }
+      boardSquares.push(<div className="board-row">{boardRow}</div>);
+    }
+
+    return (
+      <div>
+        {boardSquares}
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+        mostRecentMoveLocation: null,
+      }],
+      stepNumber: 0,
+      xIsNext: true,
+    }
+  }
+
+  handleClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares: squares,
+        mostRecentMoveLocation: calculateLocation(i),
+      }]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    })
+  }
+
+  render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const xOrO = (move % 2) === 0 ? 'O' : 'X';
+      const location = step.mostRecentMoveLocation;
+      const desc = move ?
+        `Go to move #${move} : ${xOrO} @ ${location}` :
+        'Go to game start';
+      const bold = this.state.stepNumber === move;
+      return (
+        <li key={move}>
+          <button style={{fontWeight: bold ? 'bold' : ''}} onClick={() => this.jumpTo(move)}>
+            {desc}
+          </button>
+        </li>
+      );
+    });
+
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <ol>{moves}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Game />,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+function calculateLocation(square) {
+  switch (square) {
+    case 0:
+      return '(1, 1)';
+    case 1:
+      return '(2, 1)';
+    case 2:
+      return '(3, 1)';
+    case 3:
+      return '(1, 2)';
+    case 4:
+      return '(2, 2)';
+    case 5:
+      return '(3, 2)';
+    case 6:
+      return '(1, 3)';
+    case 7:
+      return '(2, 3)';
+    case 8:
+      return '(3, 3)';
+  }
+  return null;
+}
